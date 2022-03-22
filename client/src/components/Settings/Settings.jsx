97cd2205316f1bb7b201bfe5d2fb872c3e5e9d35
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-22 21:02:17 +0530

    Update Settings.jsx

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -64,27 +64,3 @@ export default function Settings() {
             </div>
 		</div>
 	);
-
-export default function Settings() {
-  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [theme, setTheme] = useLocalStorage(
-    "theme",
-    defaultDark ? "dark" : "light"
-  );
-  const switchTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-  };
-  return (
-    <div className={styles.home} data-theme={theme}>
-      <button className={styles.switchTheme} onClick={switchTheme}>{theme}</button>
-
-      <div className={styles.sideBar}>
-        <SideBar active="settings"/>
-      </div>
-      <div className={styles.chats}>
-      </div>
-      <div className={styles.individual}></div>
-    </div>
-  );
-}
