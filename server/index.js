2a1c70a4c1929c8a60f2bcf2ec1a1de48937767f
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-23 20:18:37 +0530

    testing heroku cli

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const Gun = require('gun')
+const express = require("express");
+const Gun = require("gun");
 
-const app = express()
-const port = 8000
-app.use(Gun.serve)
+const app = express();
+const port = process.env.PORT || 8800;
+app.use(Gun.serve);
 
 const server = app.listen(port, () => {
-    console.log("Listening at: http://localhost://" + port)
-})
+  console.log("Listening at: http://localhost://" + port);
+});
 
-Gun({web: server})
+Gun({ web: server });
