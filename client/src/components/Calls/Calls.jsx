e9fa62deeab6e8c5af5750dd99577ac8a6cb7fca
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-23 19:43:50 +0530

    Server ( Initial Server)

--- client/src/components/Calls/Calls.jsx
+++ client/src/components/Calls/Calls.jsx
@@ -2,6 +2,12 @@ import styles from "../../styles/Home/Home.module.css";
 import { useLocalStorage } from "../snippets/useLocalStorage";
 
 import SideBar from "../SideBar";
+import Gun from "gun";
+import { useEffect, useState } from "react";
+
+const gun = Gun({
+  peers: ["http:localhost:8000/gun"],
+});
 
 export default function Calls() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -13,14 +19,47 @@ export default function Calls() {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
+
+  const [txt, setTxt] = useState();
+
+  useEffect(() => {
+    gun.get("text").once((node) => {
+      console.log(node);
+      if (node == undefined) {
+        gun.get("text").put({ text: "Write the text here" });
+      } else {
+        console.log("Found Node");
+        setTxt(node.text);
+      }
+    });
+
+    gun.get("text").on((node) => {
+      console.log("Receiving Update");
+      console.log(node);
+      setTxt(node.text);
+    });
+  }, []);
+
+  const updateText = (event) => {
+    console.log("Updating Text");
+    console.log(event.target.value);
+    gun.get("text").put({ text: event.target.value }); // Edit the value in our db
+    setTxt(event.target.value);
+  };
+
   return (
     <div className={styles.home} data-theme={theme}>
-      <button className={styles.switchTheme} onClick={switchTheme}>{theme}</button>
+      <button className={styles.switchTheme} onClick={switchTheme}>
+        {theme}
+      </button>
 
       <div className={styles.sideBar}>
-        <SideBar active="calls"/>
+        <SideBar active="calls" />
       </div>
       <div className={styles.chats}>
+        <div className="App">
+          <textarea value={txt} onChange={updateText} />
+        </div>
       </div>
       <div className={styles.individual}></div>
     </div>
