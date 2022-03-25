0ea003fb24ccf443733408dba57ad577e635064f
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2022-03-25 21:06:08 +0530

    Routing Fix Check #4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import Landing from "./components/Landing/Landing";
-import Home from "./components/Home/Home";
-import Notifications from "./components/Notification/Notifications";
-import Calls from "./components/Calls/Calls";
-import Favorites from "./components/Favorites/Favorites";
-import Settings from "./components/Settings/Settings";
+import App from "./App";
+
 import "./styles/index.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/calls" element={<Calls />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
-  </HashRouter>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
